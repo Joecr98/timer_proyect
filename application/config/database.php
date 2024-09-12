a4d2ad49266071af4964e3ea196b84817ddd73d5
Josué Cruz Díaz <53123993+Joecr98@users.noreply.github.com>
2024-09-12 15:21:20 -0600

    correcion de base de datos

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'port' => '3316',
+	'hostname' => 'localhost:3316',
 	'username' => 'root',
-	'password' => 'Checha680',
+	'password' => 'Test12345.',
 	'database' => 'bdtimer',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
