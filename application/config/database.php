45eea050b8cb8b2a23b217ff019fa8956e4e5104
dev.osw98cd@gmail.com <dev.osw98cd@gmail.com>
2024-09-18 22:50:40 -0600

    eliminacion de archivos vacios

--- application/config/database.php
+++ application/config/database.php
@@ -75,8 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'port' => '3316',
+	'hostname' => 'localhost:3316',
 	'username' => 'root',
 	'password' => 'Test12345.',
 	'database' => 'bdtimer',
